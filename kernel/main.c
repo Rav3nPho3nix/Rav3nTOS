e3b2ed47d22dc31d657b608a9a024a5ecfffd84b
Rav3nPho3nix <giulianocapitano@gmail.com>
2026-05-11 00:21:47 +0200

    Printing message at kernel boot

--- kernel/main.c
+++ kernel/main.c
@@ -1,13 +1,11 @@
 #include <stdint.h>
 
 #include "platform.h"
-#include "usart.h"
 
 #include "stm32h743xx.h"
 
 int main(void) {
     platform_init();
 
-    usart_putchar('O');
-    usart_putchar('K');
+    usart_write_string("OK !!");
 }
