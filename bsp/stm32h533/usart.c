a4243843991cf313e284361e25ce0aa4d84f412d
Rav3nPho3nix <giulianocapitano@gmail.com>
2026-06-04 21:35:23 +0200

    Removal of separation between 'usart' and 'usart_ll' files

--- bsp/stm32h533/usart.c
+++ bsp/stm32h533/usart.c
@@ -2,11 +2,11 @@
 
 #include <stddef.h>
 
-#include "usart_ll.h"
+#include "usart.h"
 #include "usart_types.h"
 #include "stm32h533xx.h"
 
-void usart_ll_init(uint32_t baudrate) {
+void usart_init(uint32_t baudrate) {
     // Enable USART2 clock
     RCC->APB1LENR |= RCC_APB1LENR_USART2EN;
     // Enable GPIO A clock
@@ -35,7 +35,7 @@ void usart_ll_init(uint32_t baudrate) {
     USART2->CR1 |= USART_CR1_TE | USART_CR1_RE;
 }
 
-usart_status_t usart_ll_write_char(char c) {
+usart_status_t usart_write_char(char c) {
     // If TX FIFO is full
     // if ((USART2->ISR & USART_ISR_TXE_TXFNF) == 0U) {
     //     return USART_STATUS_TX_FULL;
@@ -47,7 +47,7 @@ usart_status_t usart_ll_write_char(char c) {
     return USART_STATUS_OK;
 }
 
-usart_status_t usart_ll_read_char(char* ptr_c) {
+usart_status_t usart_read_char(char* ptr_c) {
     // If ptr_c is NULL
     if (ptr_c == NULL) {
         return USART_STATUS_ERROR;
@@ -62,7 +62,7 @@ usart_status_t usart_ll_read_char(char* ptr_c) {
     return USART_STATUS_OK;
 }
 
-usart_status_t usart_ll_write_string(const char* str) {
+usart_status_t usart_write_string(const char* str) {
     // If bad pointer
     if (str == NULL) {
         return USART_STATUS_ERROR;
@@ -70,7 +70,7 @@ usart_status_t usart_ll_write_string(const char* str) {
 
     // While the string is not empty
     while (*str != '\0') {
-        usart_status_t status = usart_ll_write_char(*str);
+        usart_status_t status = usart_write_char(*str);
 
         // Propagating the error
         if (status != USART_STATUS_OK) {
