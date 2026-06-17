9435f14d216040ee3a665b60edfe06a0cfaabd54
Rav3nPho3nix <giulianocapitano@gmail.com>
2026-06-18 00:12:11 +0200

    New version of kernel using implemented clock

--- kernel/main.c
+++ kernel/main.c
@@ -1,11 +1,22 @@
 #include <stdint.h>
 
 #include "platform.h"
+#include "log.h"
+#include "clock.h"
 
-#include "stm32h743xx.h"
+#define CLOCK_FREQUENCY 32000000
+#define TICK_FREQUENCY 1000
 
 int main(void) {
     platform_init();
 
-    usart_write_string("OK !!");
+    if (clock_init(CLOCK_FREQUENCY / TICK_FREQUENCY) != 0U) {
+        while(1);
+    }
+
+    // Kernel body
+    while(1) {
+        log_info("This is printed every second !");
+        clock_wait(1000);
+    }
 }
