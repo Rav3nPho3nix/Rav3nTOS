3ba17c0d965daac6a9d173ac6eeec3b4bc53ea4c
Rav3nPho3nix <giulianocapitano@gmail.com>
2026-08-01 18:59:50 +0200

    Removal of old function's signatures

--- includes/log.h
+++ includes/log.h
@@ -3,22 +3,18 @@
 
 // Log info
 // Take message as parameter
-// void log_info(const char*);
 void log_info(const char* format, ...);
 
 // Log error
 // Take message as parameter
-// void log_error(const char*);
 void log_error(const char* format, ...);
 
 // Warn error
 // Take message as parameter
-// void log_warn(const char*);
 void log_warn(const char* format, ...);
 
 // Panic and dont let the kernel execute furthermore
 // Take message as parameter
-// void log_panic(const char*);
 void log_panic(const char* format, ...);
 
 #endif
