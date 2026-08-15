7af18ab3648e064fe81969b78c1dcb895867d0f8
Rav3nPho3nix <giulianocapitano@gmail.com>
2026-08-15 15:14:50 +0200

    kernel/main.c

--- kernel/main.c
+++ kernel/main.c
@@ -1,88 +1,63 @@
-#include <stdint.h>
-
-// #include "platform.h"
-// #include "log.h"
+// // Kernel main function
+// #include "critical.h"
 // #include "clock.h"
 // #include "task.h"
+// #include "scheduler.h"
 
-// #define CLOCK_FREQUENCY 32000000
-// #define TICK_FREQUENCY 1000
+// void main() {
+//     critical_init();
+//     // Setup clock with 1ms
+//     // 1ms = 1 000 000 ns
+//     clock_init(1000000);
+//     task_init();
+//     scheduler_init();
+//     scheduler_start();
+//     // main function must never return
+//     while (1);
+// }
+
+/*
+This example kernel program show the usage of 'task_sleep'
+'task_sleep' put the task into SLEEPING state and give the hand to the scheduler.
+Every quantum, it will check if the sleeping time is done. If yes, the task is put to READY and can be selected again by scheduler
+
+This example only run a task that print then sleep for 5s
+*/
 
 #include "log.h"
 #include "clock.h"
 #include "task.h"
 #include "scheduler.h"
+#include "printf.h"
+#include "critical.h"
 
-#include <stdio.h>
-#include <stdbool.h>
+// Task function
+void task_function(TaskArgs args) {
+    while (1) {
+        printf("TASK OUTPUT\n");
 
-void _1() {
-    while (1)
-        printf("TASK 1 RUNNING\n");
-}
-void _2() {
-    while (1)
-        printf("TASK 2 RUNNING\n");
-}
-
-void _3() {
-    while (1)
-        printf("TASK 3 RUNNING\n");
+        // put to sleep for 5s
+        printf("TASK SLEEP FOR 5s\n");
+        task_sleep(5000);
+    }
 }
 
 int main(void) {
-    // platform_init();
-
-    // // Init clock
-    // if (clock_init(CLOCK_FREQUENCY / TICK_FREQUENCY) != 0U) {
-    //     while(1);
-    // }
-    
-    // TaskManager *tm;
-    // // Init tasks
-    // tm = task_init();
-
-    // // Kernel body
-    // while(1) {
-    //     TaskAddStatus status;
-    //     uint32_t task_id;
-
-    //     log_info("Adding a task");
-    //     status = task_add(&none, 1, &task_id);
-
-    //     if (status == TASK_ADD_STATUS_OK) {
-    //         log_warn("\t>> OK !");
-    //     }
-    //     else if (status == TASK_ADD_STATUS_ILLEGAL_PRIORITY) {
-    //         log_warn("\t>> Illegal priority !");
-    //     }
-    //     else {
-            // log_warn("\t>> No other available tasks !");
-    //     }
-
-    //     clock_wait(1000);
-    // }
-
+    critical_init();
     // Setup clock with 1ms
     // 1ms = 1 000 000 ns
     clock_init(1000000);
-
     task_init();
     scheduler_init();
 
-    TaskId task_id;
-    TaskAddStatus task_st;
+    // Add Task
+    if(task_add(&task_function, NULL, HIGHEST_PRIORITY, NULL) != TASK_ADD_STATUS_OK) {
+        log_warn("IMPOSSIBLE TO ADD TASK\n");
+    }
+    log_info("TASK ADDED");
 
-    if(task_add(&_1, 0, &task_id) == TASK_ADD_STATUS_OK) {
-        printf("ADDED TASK : id = %u, fn = %p\n", task_id.id, &_1);
-    }
-    if(task_add(&_2, 0, &task_id) == TASK_ADD_STATUS_OK) {
-        printf("ADDED TASK : id = %u, fn = %p\n", task_id.id, &_2);
-    }
-    if(task_add(&_3, 0, &task_id) == TASK_ADD_STATUS_OK) {
-        printf("ADDED TASK : id = %u, fn = %p\n", task_id.id, &_3);
-    }
-    
     scheduler_start();
-    while (1) {}
+
+    // Will never reach here
+    while (1);
 }
